2d8356fc49ab2a28a996b5d4f89b3d2db6a6e24d
sami2022skipq <abdul.sami.skipq@gmail.com>
2024-03-13 09:35:36 +0500

    login/ logout toggle

--- frontEnd/src/components/NavBar.jsx
+++ frontEnd/src/components/NavBar.jsx
@@ -18,42 +18,41 @@ const NavBar = () => {
         <Link className="p-1 rounded-md hover:bg-slate-600" to="/fav">
           Favorites
         </Link>
-        <Link className="p-1 rounded-md hover:bg-slate-600" to="/sign-in">
+        {/* <Link className="p-1 rounded-md hover:bg-slate-600" to="/sign-in">
           Log In
         </Link>
         <Link className="p-1 rounded-md hover:bg-slate-600" to="/sign-up">
           Sign Up
-        </Link>
-        <form className="flex justify-end items-end" role="search">
-          {localStorage.getItem("token") ? (
-            <>
-              <Link
-                type="button"
-                className="bg-red-700 rounded-xl p-1 hover:bg-red-900"
-                to="/sign-in"
-                role="button"
-                onClick={() => {
-                  localStorage.setItem("token", "");
-                }}
-              >
-                Log Out
-              </Link>
-            </>
-          ) : (
-            // showing Login and Sigup button based on pathname
-            <div>
-              {location.pathname === "/login" && (
-                <Link
-                  className="btn btn-primary mx-1"
-                  to="/signup"
-                  role="button"
-                >
-                  Sign Up
-                </Link>
-              )}
-            </div>
-          )}
-        </form>
+        </Link> */}
+        {/* <form className="flex justify-end items-end" role="search"> */}
+        {localStorage.getItem("token") ? (
+          <div>
+            <Link
+              type="button"
+              className="bg-red-700 rounded-xl p-1 hover:bg-red-900"
+              to="/sign-in"
+              role="button"
+              onClick={() => {
+                localStorage.setItem("token", "");
+              }}
+            >
+              Log Out
+            </Link>
+          </div>
+        ) : (
+          // showing Login and Sigup button based on pathname
+          <div>
+            {location.pathname == "/sign-up" ?(
+            <Link className="p-1 rounded-md hover:bg-slate-600" to="/sign-in">
+              Log In
+            </Link>) : (
+            <Link className="p-1 rounded-md hover:bg-slate-600" to="/sign-up">
+              Sign Up
+            </Link>
+            )}
+          </div>
+        )}
+        {/* </form> */}
       </div>
     </>
   );
